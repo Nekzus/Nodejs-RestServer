00d995b73540a228bd0a4293640f3f6780d0aabc
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-08-06 20:48:02 -0300

    Primer commit

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,14 +1,22 @@
-import { response } from "express";
+import { request, response } from "express";
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+  const { q, nombre = "No Name", apikey, page = 1, limit } = req.query;
   res.json({
     msg: "get API - controlador",
+    q,
+    nombre,
+    apikey,
+    page,
+    limit,
   });
 };
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = (req = request, res = response) => {
+  const { id } = req.params;
   res.json({
     msg: "put API - controlador",
+    id,
   });
 };
 
