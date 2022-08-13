5961b5944b514ee5b03e7f74a0477dc77ee3b5f8
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-08-13 12:40:00 -0300

    Busqueda por colecciones

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -9,10 +9,57 @@ const buscarUsuarios = async (termino = "", res = response) => {
   const esMongoID = ObjectId.isValid(termino);
   if (esMongoID) {
     const usuario = await Usuario.findById(termino);
-    res.json({
+    return res.json({
       results: usuario ? [usuario] : [],
     });
   }
+  const regex = new RegExp(termino, "i");
+  const usuarios = await Usuario.find({
+    $or: [{ nombre: regex }, { email: regex }],
+    $and: [{ estado: true }],
+  });
+  res.json({
+    results: usuarios,
+  });
+};
+
+const buscarCategorias = async (termino = "", res = response) => {
+  const esMongoID = ObjectId.isValid(termino);
+  if (esMongoID) {
+    const categoria = await Categoria.findById(termino);
+    return res.json({
+      results: categoria ? [categoria] : [],
+    });
+  }
+  const regex = new RegExp(termino, "i");
+  const categorias = await Categoria.find({
+    nombre: regex,
+    estado: true,
+  });
+  res.json({
+    results: categorias,
+  });
+};
+
+const buscarProductos = async (termino = "", res = response) => {
+  const esMongoID = ObjectId.isValid(termino);
+  if (esMongoID) {
+    const producto = await Producto.findById(termino).populate(
+      "categoria",
+      "nombre"
+    );
+    return res.json({
+      results: producto ? [producto] : [],
+    });
+  }
+  const regex = new RegExp(termino, "i");
+  const productos = await Producto.find({
+    nombre: regex,
+    estado: true,
+  }).populate("categoria", "nombre");
+  res.json({
+    results: productos,
+  });
 };
 
 const buscar = (req = request, res = response) => {
@@ -28,8 +75,10 @@ const buscar = (req = request, res = response) => {
       buscarUsuarios(termino, res);
       break;
     case "categorias":
+      buscarCategorias(termino, res);
       break;
     case "productos":
+      buscarProductos(termino, res);
       break;
 
     default:
